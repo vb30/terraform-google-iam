4c35cdb6979028a3aab07f7f5cfdf033c147a0df
vb30 <varun.94306@gmail.com>
2024-09-06 14:04:16 +0530

    Update variables.tf, add project, sa details

--- examples/service_account/variables.tf
+++ examples/service_account/variables.tf
@@ -23,13 +23,13 @@ variable "group_email" {
 variable "sa_email" {
   type        = string
   description = "Email for Service Account to receive roles (Ex. default-sa@example-project-id.iam.gserviceaccount.com)"
-  default     = "sa-tf-test-receiver-01@ci-iam-0c5f.iam.gserviceaccount.com"
+  default     = "bqdw-uat-1@drs-issue-2.iam.gserviceaccount.com"
 }
 
 variable "user_email" {
   type        = string
   description = "Email for group to receive roles (Ex. user@example.com)"
-  default     = "awmalik@google.com"
+  default     = "varunbhardwaj@google.com"
 
 }
 
@@ -39,17 +39,17 @@ variable "user_email" {
 variable "service_account_project" {
   type        = string
   description = "Project id of the service account"
-  default     = "ci-iam-0c5f"
+  default     = "drs-issue-2"
 }
 
 variable "service_account_one" {
   type        = string
   description = "First service Account to add the IAM policies/bindings"
-  default     = "sa-tf-test-01@ci-iam-0c5f.iam.gserviceaccount.com"
+  default     = "bqdw-uat-1@drs-issue-2.iam.gserviceaccount.com"
 }
 
 variable "service_account_two" {
   type        = string
   description = "First service Account to add the IAM policies/bindings"
-  default     = "sa-tf-test-02@ci-iam-0c5f.iam.gserviceaccount.com"
+  default     = "800759502819-compute@developer.gserviceaccount.com"
 }
