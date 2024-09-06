715989bc27010aecfbc5722081c2d82f46ecee3c
vb30 <varun.94306@gmail.com>
2024-09-06 17:41:29 +0530

    Update variables.tf add default vals

--- examples/project/variables.tf
+++ examples/project/variables.tf
@@ -17,16 +17,19 @@
 variable "group_email" {
   type        = string
   description = "Email for group to receive roles (ex. group@example.com)"
+  default = "scc-ai-eng@google.com"
 }
 
 variable "sa_email" {
   type        = string
   description = "Email for Service Account to receive roles (Ex. default-sa@example-project-id.iam.gserviceaccount.com)"
+  default = "bqdw-uat-1@drs-issue-2.iam.gserviceaccount.com"
 }
 
 variable "user_email" {
   type        = string
   description = "Email for group to receive roles (Ex. user@example.com)"
+  default = "varunbhardwaj@google.com"
 }
 
 /******************************************
@@ -35,10 +38,12 @@ variable "user_email" {
 variable "project_one" {
   type        = string
   description = "First project id to add the IAM policies/bindings"
+  default = "drs-issue-2"
 }
 
 variable "project_two" {
   type        = string
   description = "Second project id to add the IAM policies/bindings"
+  default = "test-blueprint-deployment"
 }
 
